642b657349f1d25f6279f8295baca6d19e93e7ae
Omar Laurino <olaurino@cfa.harvard.edu>
2015-04-19 15:54:58 -0400

    add some docstrings

--- volib/__init__.py
+++ volib/__init__.py
@@ -1,24 +1,45 @@
 __author__ = 'olaurino'
 
-__all__ = ['Resolver', 'resolvers', 'resolve', 'add_resolver', 'vodml', 'get_object', 'Context', 'get_context']
+__all__ = ['Resolver', 'resolvers', 'resolve', 'add_resolver', 'vodml', 'get_object', 'Context', 'get_context',
+           'vodmlref_factory']
 
 from six import string_types
 from . import vodml_ref
 
-def utype_factory(utype):
-    if isinstance(utype, string_types):
-        utype = vodml_ref.VODML_REF(utype)
-    return utype
+def vodmlref_factory(vodmlref):
+    """
+    Build a :class:`~vodml_ref.VODML_REF` object from a string,
+    or return the object untouched if the object is already an
+    instance of :class:`~vodml_ref.VODML_REF`
+
+    :param vodmlref: a vodml_ref string to convert or a :class:`~vodml_ref.VODML_REF` instance to pass through.
+    :type vodmlref: basestring or :class:`~vodml_ref.VODML_REF`
+    :return: a :class:`~vodml_ref.VODML_REF` instance
+    :rtype: :class:`~vodml_ref.VODML_REF`
+    """
+    if isinstance(vodmlref, string_types):
+        vodmlref = vodml_ref.VODML_REF(vodmlref)
+    return vodmlref
 
 
 class Resolver(object):
+    """
+    Base class for VODML resolvers.
+    """
     def __init__(self):
         self._resolver = {}
         self.name = None
         self.version = None
 
     def resolve(self, utype):
-        utype = utype_factory(utype)
+        """
+        Blah Blah
+        :param utype:
+        :type utype:
+        :return:
+        :rtype:
+        """
+        utype = vodmlref_factory(utype)
         vodml_id = utype.vodml_id
         return self._resolver[vodml_id]
 
@@ -33,12 +54,12 @@ for ep in iter_entry_points(group='vodml.resolver'):
 
 
 def resolve(utype, version):
-    utype = utype_factory(utype)
+    utype = vodmlref_factory(utype)
     return resolvers[(utype.prefix, version)].resolve(utype)
 
 
 def get_object(utype, version):
-    utype = utype_factory(utype)
+    utype = vodmlref_factory(utype)
     resolver = resolvers[(utype.prefix, version)]
     full_path = resolver.resolve(utype)
     ret = __import__(resolver.root_pkg)
@@ -57,13 +78,13 @@ class Context(object):
         self.models = {model.name: model.version for model in models}
 
     def resolve(self, utype):
-        utype = utype_factory(utype)
+        utype = vodmlref_factory(utype)
         name = utype.prefix
         version = self.models[name]
         return resolve(utype, version)
 
     def get_object(self, utype):
-        utype = utype_factory(utype)
+        utype = vodmlref_factory(utype)
         name = utype.prefix
         version = self.models[name]
         return get_object(utype, version)
